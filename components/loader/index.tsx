eec4b179f2c650542c2da682f1256155fca3c1fa
Z1n <tv.noob@bk.ru>
2024-09-01 19:45:42 +0700

    loader base

---
+++ components/loader/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/style.css'
+
+const loader = () => {
+    return (
+        <>
+            <div className="loader-area">
+                <div className={"loader"}></div>
+            </div>
+        </>
+    );
+};
+
+export default loader;
